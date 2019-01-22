d2bc80c83f71ce76d55990150fde62d191988e15
Raniro Coelho <errecme@gmail.com>
2019-01-22 20:53:46 -0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.sass';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          heu
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
